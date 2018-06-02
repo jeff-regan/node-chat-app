2f09c753f564650ee15babe8c469be0ac4a1cbec
jeff regan <jeff@jkregan.com>
2018-06-02 10:48:21 -0600

    Added brodcast and welcome notifications.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const http = require('http');
+
 const express = require('express');
+const socketIO = require('socket.io');
 
 // console.log(__dirname + '/../public');
 // console.log(path.join(__dirname, '../public'));
@@ -8,8 +11,61 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat!',
+		createdAt: new Date().getTime(),
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime(),
+	});
+
+	socket.on('createMessage', (newMessage) => {
+		io.emit('newMessage', {
+			from: newMessage.from,
+			text: newMessage.text,
+			createdAt: new Date().getTime(),
+		});
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+	});
+});
+
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
+
+
+// socket.emit('newMessage', {
+// 	from: 'Admin',
+// 	text: 'Welcome to the chat app',
+// 	createdAt: new Date().getTime()
+// });
+
+// socket.broadcast.emit('newMessage', {
+// 	from: 'Admin',
+// 	text: 'New user joined',
+// 	createdAt: new Date().getTime()
+// });
+
+// socket.on('createMessage', (message) => {
+// 	console.log('createMessage', message);
+// 	io.emit('newMessage', {
+// 		from: message.from,
+// 		text: message.text,
+// 		createdAt: new Date().getTime()
+// 	});
