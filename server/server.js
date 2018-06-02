ef84e13c88444270847e73b74ca66f9c0626ba39
jeff regan <jeff@jkregan.com>
2018-06-02 13:33:25 -0600

    added generateMessage utility

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-// console.log(__dirname + '/../public');
-// console.log(path.join(__dirname, '../public'));
-
+const  {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -17,26 +15,12 @@ const io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-	console.log('New user connected');
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-	socket.emit('newMessage', {
-		from: 'Admin',
-		text: 'Welcome to the chat!',
-		createdAt: new Date().getTime(),
-	});
-
-	socket.broadcast.emit('newMessage', {
-		from: 'Admin',
-		text: 'New user joined',
-		createdAt: new Date().getTime(),
-	});
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 	socket.on('createMessage', (newMessage) => {
-		io.emit('newMessage', {
-			from: newMessage.from,
-			text: newMessage.text,
-			createdAt: new Date().getTime(),
-		});
+		io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
 	});
 
 	socket.on('disconnect', () => {
@@ -44,28 +28,6 @@ io.on('connection', (socket) => {
 	});
 });
 
-
 server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
-
-
-// socket.emit('newMessage', {
-// 	from: 'Admin',
-// 	text: 'Welcome to the chat app',
-// 	createdAt: new Date().getTime()
-// });
-
-// socket.broadcast.emit('newMessage', {
-// 	from: 'Admin',
-// 	text: 'New user joined',
-// 	createdAt: new Date().getTime()
-// });
-
-// socket.on('createMessage', (message) => {
-// 	console.log('createMessage', message);
-// 	io.emit('newMessage', {
-// 		from: message.from,
-// 		text: message.text,
-// 		createdAt: new Date().getTime()
-// 	});
