e282d345dba3198ba9ea26e868d48b972decc501
jeff regan <jeff@jkregan.com>
2018-06-03 13:47:40 -0600

    styled the chat app

--- public/js/index.js
+++ public/js/index.js
@@ -21,28 +21,36 @@ socket.on('newLocationMessage', function(message) {
 });
 
 $('#message-form').on('submit', function(e) {
+	const messageTextbox = $('input[name=message]')
 	e.preventDefault();
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('input[name=message]').val(),
+		text: messageTextbox.val(),
 	}, function() {
-		$('input[name=message]').val('');
+		$messageTextbox.val('');
 	});
 });
 
-$('#send-location').on('click', function(e) {
+let locationButton = $('#send-location');
+let locationButtonLabel = locationButton.text();
+
+locationButton.on('click', function(e) {
 	e.preventDefault();
 	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
+
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
 	navigator.geolocation.getCurrentPosition(
 		function(position) {
+			locationButton.removeAttr('disabled').text(locationButtonLabel);
 			socket.emit('createLocationMessage', {
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude,
 			});
 		},
 		function() {
+			locationButton.removeAttr('disabled').text(locationButtonLabel);
 			alert('Unable to fetch location. Boo Hoo');
 		}
 	);
